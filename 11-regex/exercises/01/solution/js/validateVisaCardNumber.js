76f36971e013dc6baa74f9f51c58df72db397739
Kawi <kawiwinter@gmail.com>
2022-08-27 00:24:26 +0200

    all tests passed

--- 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
+++ 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
@@ -1,1 +1,8 @@
+"use strict";
+
+const validateVisaCardNumber = (str) => {
+  const regExpVisa = /^4[0-9]{5}([0-9]{6}|[0-9]{9})[0-9]{4}$/;
+  return str.match(regExpVisa) ? true : false;
+};
+
 export default validateVisaCardNumber;
--- 11-regex/exercises/02/solution/js/validateIPAddress.js
+++ 11-regex/exercises/02/solution/js/validateIPAddress.js
@@ -1,1 +1,9 @@
+"use strict";
+
+const validateIPAddress = (IPv4) => {
+  const regExpIPv4 =
+    /^(((1[0-9]{0,2})|(2[0-5]{2})|(2[0-9]{1})|([3-9][0,9]{2}))([.-])){1}((0|(1[0-9]{0,2})|(2[0-5]{2})|(2[0-9]{1})|([3-9][0,9]{2}))([.-])){2}((0|1[0-9]{0,2})|(2[0-5]{2})|(2[0-9]{1})|([3-9][0,9]{2}))$/;
+  return IPv4.match(regExpIPv4) ? true : false;
+};
+
 export default validateIPAddress;
